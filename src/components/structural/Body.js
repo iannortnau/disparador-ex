6296cdee79e6c680d185b729d407f2d39632d5c5
iannortnau <ortnau.iann@gmail.com>
2022-06-23 16:38:25 -0300

    primeira vercao finalizada

--- src/components/structural/Body.js
+++
@@ -1,9 +0,0 @@
-import styles from '../../styles/components/Body.module.css'
-
-export default function Body(props) {
-  return (
-      <div className={styles.body}>
-          {props.children}
-      </div>
-  );
-}
